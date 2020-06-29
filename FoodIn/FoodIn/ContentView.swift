fba9175a30c7afdf2231bcdb45a57478045e6664
Sabrina Zhao <sabrinazhao@Sabrinas-MacBook-Pro-2.local>
2020-06-28 19:48:02 -0700

    initial commit

---
+++ FoodIn/FoodIn/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  FoodIn
+//
+//  Created by Sabrina Zhao on 2020/6/28.
+//  Copyright © 2020 FoodIn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
